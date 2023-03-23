685db3f9ba846e3344db5d4865054212e67e9071
amitsatpute-gslab <amitkumar.satpute@gslab.com>
2023-03-23 15:02:19 +0530

    swagger ui api doc added

--- src/app/express/index.ts
+++ src/app/express/index.ts
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { router } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import { apiDoc } from "./openapi/products-api";
 
 dotenv.config();
 const port = process.env.PORT || 8082;
 
 const app = express();
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(apiDoc));
 app.use(express.json());
 app.use(cors());
 app.use(router);
